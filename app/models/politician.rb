e93a7d6ad3698705cc90bf500b808e866f6ccf34
Andy Tiffany <andy.tiffany@twintechs.com>
2016-10-14 12:13:36 -0700

    adds test and has_many association for politicians to staff_people

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,2 +1,3 @@
 class Politician < ApplicationRecord
+  has_many :staff_people
 end
--- test/models/politician_test.rb
+++ test/models/politician_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PoliticianTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @hillary = Politician.new(name: "Hillary", party: "democrat")
+  end
+
+  test "can add staff" do
+    robby = StaffPerson.new(name: "Robby Mook", politician: @hillary)
+    robby.save
+
+    assert_equal 1, @hillary.staff_people.size
+  end
 end
